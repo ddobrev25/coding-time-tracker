2566f276fec2b133df0aa6945cb455f5ad8e644e
Daniel Dobrev <danidobrev2005@gmail.com>
2023-01-27 22:49:32 +0200

    Finished time trigger type.

--- Coding Time Tracker/Program.cs
+++ Coding Time Tracker/Program.cs
@@ -5,3 +5,12 @@ using Coding_Time_Tracker.Services;
 
 
 string fileName = "file.cttf";
+
+TimeTrigger tt = new TimeTrigger(5);
+tt.Triggered += () =>
+{
+    Console.WriteLine("triggered");
+};
+
+Thread.Sleep(7000);
+Thread.Sleep(100000);
--- Coding Time Tracker/TimeTrigger.cs
+++ Coding Time Tracker/TimeTrigger.cs
@@ -7,47 +7,106 @@ using System.Threading;
 
 namespace Coding_Time_Tracker
 {
+    /// <summary>
+    /// Provides a disposable object that will trigger periodically with a given delay.
+    /// </summary>
     public class TimeTrigger : IDisposable
     {
+        /// <summary>
+        /// The time of the next trigger.
+        /// </summary>
         public DateTime TriggerTime
         {
-            get 
-            { 
-                return DateTime.Now + _triggerAfter; 
-            }
+            get { return _triggerTime; }
         }
 
-        private CancellationTokenSource? _tokenSource = null;
 
-        private Task? _runningTask = null;
 
-        private TimeSpan _triggerAfter = TimeSpan.Zero;
-
+        /// <summary>
+        /// Initiator
+        /// </summary>
+        /// <param name="seconds">The ammout of seconds to delay.</param>
         public TimeTrigger(int seconds)
         {
-            _triggerAfter = TimeSpan.FromSeconds(seconds);
-            Start();
-        }
-        public TimeTrigger(int minutes, int seconds)
-        {
-            _triggerAfter = new TimeSpan(0, minutes, seconds);
-            Start();
-        }
-        public TimeTrigger(int hours, int minutes, int seconds)
-        {
-            _triggerAfter = new TimeSpan(hours, minutes, seconds);
-            Start();
-        }
-        public TimeTrigger(TimeSpan triggerAfter)
-        {
-            _triggerAfter = triggerAfter;
+            _triggerDelay = TimeSpan.FromSeconds(seconds);
             Start();
         }
 
+
+        /// <summary>
+        /// Initiator
+        /// </summary>
+        /// <param name="minutes">The ammout of minutes to delay.</param>
+        /// <param name="seconds">The ammout of seconds to delay.</param>
+        public TimeTrigger(int minutes, int seconds)
+        {
+            _triggerDelay = new TimeSpan(0, minutes, seconds);
+            Start();
+        }
+
+
+        /// <summary>
+        /// Initiator
+        /// </summary>
+        /// <param name="hours">The ammout of hours to delay.</param>
+        /// <param name="minutes">The ammout of minutes to delay.</param>
+        /// <param name="seconds">The ammout of seconds to delay.</param>
+        public TimeTrigger(int hours, int minutes, int seconds)
+        {
+            _triggerDelay = new TimeSpan(hours, minutes, seconds);
+            Start();
+        }
+
+
+        /// <summary>
+        /// Initiator
+        /// </summary>
+        /// <param name="triggerDelay">Delay in TimeSpan format.</param>
+        public TimeTrigger(TimeSpan triggerDelay)
+        {
+            _triggerDelay = triggerDelay;
+            Start();
+        }
+
+        /// <summary>
+        /// Stops and disposes the object.
+        /// </summary>
+        public void Stop()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            _tokenSource?.Cancel();
+            _tokenSource?.Dispose();
+            _tokenSource = null;
+            _runningTask?.Dispose();
+            _runningTask = null;
+        }
+
+        /// <summary>
+        /// Disposes the object when out of scope.
+        /// </summary>
+        ~TimeTrigger() => Dispose();
+
+
+        /// <summary>
+        /// Triggers periodically with the specified delay.
+        /// </summary>
+        public event Action? Triggered;
+
+        /// <summary>
+        /// Handler for Triggered event.
+        /// </summary>
+        protected virtual void OnTriggered()
+        {
+            Triggered?.Invoke();
+        }
 
         private void Start()
         {
-            if (_triggerAfter < TimeSpan.Zero)
+            if (_triggerDelay < TimeSpan.Zero)
             {
                 throw new InvalidDataException();
             }
@@ -59,40 +118,35 @@ namespace Coding_Time_Tracker
 
             _tokenSource = new CancellationTokenSource();
 
+            SetTriggerTime();
+
             _runningTask = Task.Factory.StartNew(async () =>
             {
                 while (true)
                 {
-                    if (TriggerTime <= DateTime.Now)
+                    await Task.Delay(1000, _tokenSource.Token);
+                    if (_triggerTime <= DateTime.Now)
                     {
                         OnTriggered();
-                        Dispose();
-                        return;
+                        SetTriggerTime();
                     }
-                    await Task.Delay(1000, _tokenSource.Token);
                 }
             }, _tokenSource.Token);
         }
-        public void Cancel()
+
+
+        private void SetTriggerTime()
         {
-            Dispose();
+            _triggerTime = DateTime.Now + _triggerDelay;
         }
 
-        protected virtual void OnTriggered()
-        {
-            Triggered?.Invoke();
-        }
 
-        public void Dispose()
-        {
-            _tokenSource?.Cancel();
-            _tokenSource?.Dispose();
-            _tokenSource = null;
-            _runningTask?.Dispose();
-            _runningTask = null;
-        }
-        ~TimeTrigger() => Dispose();
+        private DateTime _triggerTime;
 
-        public event Action? Triggered;
+        private TimeSpan _triggerDelay;
+
+        private CancellationTokenSource? _tokenSource = null;
+
+        private Task? _runningTask = null;
     }
 }
